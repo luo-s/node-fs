b2eddaa8e9c40087efbbdd4e197abe686bf94a4a
Shu <luo.s@outlook.com>
2024-04-08 21:48:53 -0400

    add readFile.js

---
+++ readFile.js
@@ -0,0 +1,40 @@
+import fs, { readFile } from "node:fs";
+const path = "./test.txt";
+
+// readFile synchronously
+export function readFileSync(path) {
+  console.log("\nfs.readFileSync() read synchronously");
+  console.log("no need to open and close file");
+  let data = fs.readFileSync(path, "utf8");
+  console.log(`content: ${data}`);
+}
+readFileSync(path);
+
+// readFile with callback
+export function readFileAsync(path) {
+  console.log("\nfs.readFile() read asynchronously with callback");
+  console.log("no need to open and close file");
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`content: ${data}`);
+    }
+  });
+}
+readFileAsync(path);
+
+// readFile with promise
+export function readFilePromise(path) {
+  console.log("\nfs.promises.readFile() read asynchronously with promise");
+  console.log("no need to open and close file");
+  fs.promises
+    .readFile(path, "utf8")
+    .then((data) => {
+      console.log(`content: ${data}`);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+readFilePromise(path);
