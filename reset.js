f5c796dfb0bf9ed8706679551d9e43d2b34db4b7
Shu <luo.s@outlook.com>
2024-04-10 11:15:43 -0400

    modulize reset method

--- reset.js
+++ reset.js
@@ -1,9 +1,12 @@
 import fs from "node:fs";
 const path = "./test.txt";
-const string = "This is a test.";
+const string = "This is a test file.";
 
 // write synchronously with string
-let fd = fs.openSync(path, "w");
-let bytesWritten = fs.writeSync(fd, string, "utf8");
-console.log("file reset!");
-fs.closeSync(fd);
+export var reset = (path, data) => {
+  let fd = fs.openSync(path, "w");
+  let bytesWritten = fs.writeSync(fd, data, "utf8");
+  console.log("file reset!");
+  fs.closeSync(fd);
+};
+reset(path, string);
